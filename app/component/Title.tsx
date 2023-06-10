10114e06b2d7e61b0715130d687198479c5c073e
Risini Piyathma <62292463+Risini1998@users.noreply.github.com>
2023-06-10 18:40:15 +0530

    Update Title.tsx

--- app/component/Title.tsx
+++ app/component/Title.tsx
@@ -1,7 +1,7 @@
 export default function Title() {
   return (  
       <div className="text-center font-inter">
-        <h1 className=' font-extrabold text-title/[44px] tracking-tight'>Good Morning, Johny!</h1>
+        <h1 className=' font-extrabold text-title/[44px] tracking-tight'>Good , Johny!</h1>
         <h3 className=' font-medium text-subtitle/[22px] tracking-tight m-3'>🌤   It&apos;s better to go shopping before this friday</h3>
       </div>
     )
